2b7c49f9537831eae4820983fbd7ffb86dfc25ef
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-27 17:36:09 +0700

    Add Clang setup in setup_colab.sh
    Download and extract Clang in the kernelbuild_kaggle_script directory.

--- setup_colab.sh
+++ setup_colab.sh
@@ -20,6 +20,7 @@ binutils-aarch64-linux-gnu \
 patch \
 ccache
 #clang setup
+cd kernelbuild_kaggle_script
 wget https://github.com/greenforce-project/greenforce_clang/releases/download/20260523/gf-clang-23.0.0-20260523.tar.gz;wait; mkdir -p clang; mv gf-clang-23.0.0-20260523.tar.gz clang.tar.gz;mv clang.tar.gz clang;cd clang;tar -xvzf clang.tar.gz;wait
 export PATH=$(pwd)/kernelbuild_kaggle_script/clang/bin:$PATH; clang --version
 clang --version

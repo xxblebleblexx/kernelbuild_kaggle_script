feb6f660e0f0cacddb17d972166dd1f09ce36e73
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-27 17:27:40 +0700

    Modify kernel_build.sh for new branch and settings
    Updated kernel build script to change branch and configuration settings.

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,19 +1,27 @@
 #CONFIGURATION
 kernelsource=https://github.com/xxblebleblexx/moonbeam_gale_kernel.git # Must be edited
 kernelname=$(basename "$kernelsource" .git) # No need to edit
-branch_kernel=moonbeam # Must be edited
+branch_kernel=mb-mglru # Must be edited
 defconfig_path=arch/arm64/configs/gale_defconfig # Must be edited
-defconfig=${defconfig_path##*/}
-clang_path=/content/clang/bin # No need to edit
-fast_path=/tmp # This where kernelsource saved
-hooks=kprobes #only manual hook/kprobes hook, must be edited
+defconfig=gale_defconfig # Must be edited
+fast_path=$(pwd) # This where kernelsource saved
+hooks=manual #only manual hook/kprobes hook, must be edited
+clang=$fast_path/clang/bin
+susfs=y # only 4.19 y/n or u can change another susfs patch
 
+export PATH=$clang:$PATH
 cd $fast_path
 git clone -b $branch_kernel --depth=1 $kernelsource;wait
 cd $fast_path/$kernelname
 
 #KSU DRIVER
+if [ "$susfs" = "n" ]; then
 curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/MultiSU/refs/heads/legacy/kernel/setup.sh" | bash -s legacy
+fi
+
+if [ "$susfs" = "y" ]; then
+curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/MultiSU/refs/heads/legacy/kernel/setup.sh" | bash -s legacy_susfs
+fi
 
 #KSU ACTIVATION
 echo "CONFIG_KSU=y" >> $defconfig_path
@@ -31,10 +39,20 @@ echo "CONFIG_KSU_MANUAL_HOOK=y" >> $defconfig_path
 wget https://raw.githubusercontent.com/xxblebleblexx/manual_hook_fix/refs/heads/main/manualhook_1.6_fixed.patch;wait;patch -p1 < manualhook_1.6_fixed.patch
 fi
 
-export PATH=$clang_path:$PATH
-
-if [ -f build.sh ]; then
-chmod +x build.sh;sh build.sh;wait;cp /out/arch/arm64/boot/image.gz /content/ ;cp /out/arch/arm64/boot/image.gz-dtb /content/; cp /out/arch/arm64/boot/image /content/
-else
-make O=out ARCH=arm64 $defconfig && make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
+if [ "$susfs" = "y" ]; then
+wget https://raw.githubusercontent.com/xxblebleblexx/susfs_patch/refs/heads/mainline/susfix-419.patch;wait;patch -p1 < susfix-419.patch
+echo "CONFIG_KSU_SUSFS=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=n" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_ENABLE_LOG=n" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SUS_MAP=y" >> $defconfig_path
 fi
+
+make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
+wait
